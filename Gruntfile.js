2d2bd34e3fab4f2544b101bc5e201a675781bb8e
d4i <d.narsil@gmail.com>
2013-08-12 01:20:01 +0900

    Minify html

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
 };
