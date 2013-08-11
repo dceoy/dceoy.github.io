ef669a3319598e6147a638f32e595d52aafc96a4
d4i <d.narsil@gmail.com>
2013-08-12 02:49:27 +0900

    Replace livereloader with grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,30 @@
 'use strict';
-var path = require('path');
-var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-var folderMount = function folderMount(connect, point) {
-  return connect.static(path.resolve(point));
-};
 
 module.exports = function (grunt) {
-  // Project configuration.
+
   grunt.initConfig({
-    livereload: {
-      port: 35729 // Default livereload listening port.
-    },
     connect: {
       livereload: {
         options: {
-          port: 9000,
-          middleware: function(connect, options) {
-            return [lrSnippet, folderMount(connect, options.base)]
-          }
+          port: 9000
         }
       }
     },
-    // Configuration to be run (and then tested)
-    regarde: {
+    watch: {
+      options: {
+        // Start a live reload server on the default port 35729
+        livereload: true,
+      },
       all: {
         files: ['**/*.html', '**/*.css', '**/*.js'],
-        tasks: ['livereload']
+        tasks: []
       }
     }
-
   });
 
-  grunt.loadNpmTasks('grunt-regarde');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-livereload');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+  grunt.registerTask('default', ['connect', 'watch']);
+
 };
